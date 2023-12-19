b47d81e1874b8d9e529ca5c2e82e1aadcd84aa86
Godares0410 <godares0410@gmail.com>
2023-12-19 16:19:48 +0700

    updates

--- app/Models/SesiJadwalUjian.php
+++ app/Models/SesiJadwalUjian.php
@@ -10,29 +10,10 @@ class SesiJadwalUjian extends Model
     protected $table = 'sesi_jadwal_ujian';
 
     // Sesuaikan atribut-atribut yang diperlukan
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-    protected $fillable = ['id_jadwal_ujian', 'id_sesi', 'id_alokasi_waktu'];
-=======
-=======
->>>>>>> 25eed0c (first commitz)
-=======
->>>>>>> e8f7dd6 (first commit)
     protected $fillable = [
         'id_jadwal_ujian',
         'sesi',
         'jam_mulai',
         'jam_selesai',
     ];
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> 9f5d545 (first commitu)
-=======
->>>>>>> 25eed0c (first commitz)
-=======
-=======
-    protected $fillable = ['id_jadwal_ujian', 'id_sesi', 'id_alokasi_waktu'];
->>>>>>> 680cd4c (first commit)
->>>>>>> e8f7dd6 (first commit)
 }
--- app/Models/WebsiteBeranda.php
+++ app/Models/WebsiteBeranda.php
@@ -1,68 +1,5 @@
-<<<<<<< HEAD
-=======
 <?php
 
-<<<<<<< HEAD
-namespace App\Http\Controllers;
-
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Validator;
-use App\Models\WebsiteBeranda;
-
-class WebsiteController extends Controller
-{
-    public function index()
-    {
-        $beranda = WebsiteBeranda::all();
-        return view('website.landing.index', compact('beranda'));
-    }
-    public function beranda()
-    {
-        $beranda = WebsiteBeranda::all();
-        return view('data_website.beranda.index', compact('beranda'));
-    }
-    public function berandastore(Request $request)
-    {
-
-        // Validasi input
-        $validator = Validator::make($request->all(), [
-            // 'nama_siswa' => 'required',
-            // 'kelas' => 'required',
-            // 'jurusan' => 'required',
-            'foto' => 'image|mimes:jpeg,png,jpg',
-        ]);
-
-        if ($validator->fails()) {
-            $errors = $validator->errors();
-
-            if ($errors->has('foto')) {
-                $errorMessage = 'Gagal Upload, ukuran maksimal foto 2Mb';
-                session()->flash('error', $errorMessage);
-            }
-
-            return redirect()->back()->withErrors($validator)->withInput();
-        }
-
-        $web = new WebsiteBeranda;
-        $web->tag = $request->tag;
-        $web->keterangan = $request->keterangan;
-
-        if ($request->hasFile('foto')) {
-            $foto = $request->file('foto');
-            // $namaFoto = $foto->getClientOriginalName();
-            $namaFoto = $request->nama_siswa . '_' . time() . '.' . $request->foto->extension();
-            $foto->move(public_path('img/website/landing'), $namaFoto);
-            $web->foto = $namaFoto;
-        }
-        $web->save();
-        if (session()->has('error')) {
-            $errorMessage = session()->get('error');
-            session()->forget('error');
-            return redirect()->back()->with('error', $errorMessage);
-        }
-        return redirect()->route('website.beranda')->with('success', 'Data berhasil ditambahkan.');
-    }
-=======
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -75,6 +12,4 @@ class WebsiteBeranda extends Model
     protected $guarded = 'id_beranda';
     protected $table = 'website_beranda';
     protected $primaryKey = 'id_beranda';
->>>>>>> 680cd4c (first commit)
 }
->>>>>>> e8f7dd6 (first commit)
